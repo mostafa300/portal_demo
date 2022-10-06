4bc9e68a7e3159a5811e706ec4ef8d1b5a386a0c
mostafa mahmoud <mostafa300mmm@gmail.com>
2022-10-06 15:25:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SignInSide from './pages/SignInSide';
+
+const App = () => {
+  return (
+    <>
+    <SignInSide />
+    </>
+  );
+}
+
+export default App;
