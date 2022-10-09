4fae87d86af0d04bf8410ed71e911e5f2c4c209c
Your Name <halavva@gmail.com>
2022-10-09 16:40:22 +0200

    Add profile route

---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = (props) => {
+
+    return (
+        <div>
+            <h1>This is Profile page</h1>
+        </div>
+    );
+};
+export default Profile;
